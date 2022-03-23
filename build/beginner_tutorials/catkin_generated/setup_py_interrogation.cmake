0da01b47fcea595f411c55bb2ef8a8c07669d4a3
bjb7203 <bjb7203@rit.edu>
2022-03-23 17:33:37 -0400

    complete workspace for ROS + Unity

--- build/beginner_tutorials/catkin_generated/setup_py_interrogation.cmake
+++
@@ -1,7 +0,0 @@
-set(beginner_tutorials_SETUP_PY_SETUP_MODULE "distutils.core")
-set(beginner_tutorials_SETUP_PY_VERSION "0.0.0")
-set(beginner_tutorials_SETUP_PY_SCRIPTS "")
-set(beginner_tutorials_SETUP_PY_PACKAGES "beginner_tutorials")
-set(beginner_tutorials_SETUP_PY_PACKAGE_DIRS "include/beginner_tutorials")
-set(beginner_tutorials_SETUP_PY_MODULES "")
-set(beginner_tutorials_SETUP_PY_MODULE_DIRS "")
--- devel/share/common-lisp/ros/beginner_tutorials/srv/_package_AddTwoInts.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package beginner_tutorials-srv)
-(cl:export '(EMOTION-VAL
-          EMOTION
-          SENSOR_ID-VAL
-          SENSOR_ID
-          RESP-VAL
-          RESP
-))
